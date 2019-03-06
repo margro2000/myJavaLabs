3d68c10bb5f4025b0f3688337a65213c749e54fe
Margarita Groisman <mg@Margaritas-MacBook-Pro.local>
2019-03-06 11:10:02 -0500

    new notes

---
+++ Lesson14.java
@@ -0,0 +1,27 @@
+/*
+Running time: best case, worst case, and average case
+Programmers focus on worst case
+
+T(n) = O(nO
+
+T(n) is actual running time.
+Big O is your worst TestCase
+
+O(g(n)) = f(n) such that there exists a positive contant C and
+
+for ( =0 to n)
+upper boud is n
+
+*/
+
+public class Lesson14 {
+	public int compareTo(Object o)
+}
+//comparable is a built in interface
+public class Account implements Comparables {
+	public into compareTo(Object o) {
+		//objects can be put in order
+	}
+}
+
+//The iterator interface
---
+++ Recitation8.java
@@ -0,0 +1,11 @@
+/*
+Class cast exceptions happen when
+new B() must be an instanceof A
+can use instanceof checks
+
+Dyniamic binding:
+	-if the class has the static type, then it will call they dynamic's types method
+
+Interface:
+	-has a/does a
+*/
